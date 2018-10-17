59f266d5c1a5ed83dc413f78e930102aa4e4b85f
Jonny Mooneyham <jonathan.mooneyham@gmail.com>
2018-10-16 21:07:55 -0700

    figuring stuff out

--- src/components/sandboxes/JonnySandbox.vue
+++ src/components/sandboxes/JonnySandbox.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="jonny-sandbox">
         <h1>Jonny's Dev Area</h1>
+        <France></France>
     </div>
 </template>
 
 <script>
+    import France from "../visualizations/France";
     export default {
         name: "JonnySandbox",
+        components: {France},
     }
+
 </script>
 
 <style scoped>
--- src/components/visualizations/France.vue
+++ src/components/visualizations/France.vue
@@ -1,15 +1,41 @@
 <template>
-    <div>
-
-    </div>
+        <svg width="1000" height="1000">
+            <circle v-for="(item, index) in dataset"
+                    :key="index"
+                    :cx="item[0]"
+                    :cy="item[1]"
+                    r="10"
+                    @click="onClick(item)"></circle>
+        </svg>
 </template>
 
 <script>
+    // import dataset from '../../data/france.tsv'
+    import * as d3 from 'd3'
+    import dataset from './dataset'
+
+    d3.tsv("../../data/france.tsv");
+
     export default {
-        name: "France"
+
+        data () {
+            return {
+                dataset
+            }
+        },
+        methods: {
+            onClick(item) {
+                self.console.log('hey there');
+                self.console.log(item)
+            }
+        }
     }
+
 </script>
 
-<style scoped>
+<style>
+    svg circle {
+        fill: aqua;
+    }
 
 </style>

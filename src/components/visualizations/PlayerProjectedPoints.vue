5058802fede9fd22f88b76b0dad496ea6f46e5ee
Scott Waldron <waldros2@wwu.edu>
2018-11-02 17:37:47 -0700

    Added tooltip to projection data

--- src/components/visualizations/PlayerProjectedPoints.vue
+++ src/components/visualizations/PlayerProjectedPoints.vue
@@ -32,8 +32,9 @@
         name: "PlayerProjectedPoints",
 
         mounted() {
+            
             // Establish variables
-            let player = "Kerryon Johnson";
+            const player = 'Kerryon Johnson';
 
             let xFrame = 1000,
                 yFrame = 500;
@@ -44,7 +45,7 @@
 
             let color = d3.scaleOrdinal(d3.schemeDark2);
             
-            //Establish svg variable
+            // Establish svg variable
             let svg = d3.select(this.$el)
                         .append('svg')
                         .attr('width', width + margin.left + margin.right)
@@ -52,134 +53,16 @@
                         .append("g")
                         .attr('transform', 'translate(' + margin.left + ',' + margin.top +')');
 
-            
-
-            d3.json('data/PlayerData.json')
-                .then(function (data) {
-
-                    let playerObj = data[player];
-                    let playerWeeks = playerObj.Weeks;
-                    let dataPoints = playerWeeks.length
-
-                    playerWeeks.forEach(function(d) {
-                        d.Week              = +d.Week;
-                        d.ProjectedPoints   = d.ProjectedPoints == '--' ? 'BYE' : +d.ProjectedPoints;
-                        d.AppliedPoints     = d.AppliedPoints == "--" ? 0 : +d.AppliedPoints;
-                        d.PercentError      = d.PercentError =='--' ? 0 : +d.PercentError;
-                        d.RunningError      = +d.RunningError;
-                    })
-                    let maxPoints = d3.max(playerObj.Weeks, function(d){
-                        return d.ProjectedPoints > d.AppliedPoints ? d.ProjectedPoints : d.AppliedPoints;
-                    }) * 1.10
-                    let x = d3.scaleLinear()
-                                    .domain([0, dataPoints])
-                                    .range([0, width]);
-                    let y = d3.scaleLinear()
-                                    .domain([0, maxPoints])
-                                    .range([height, 0]);
-                    
-                    svg.append("text")
-                        .attr("x", (width / 2))
-                        .attr("y", 0 - (margin.top /2 ))
-                        .attr("text-anchor", "middle")
-                        .text(player + ', '  + playerObj.Posistion + ' - ' + playerObj.Team)
-                        .attr("class", "chart-title");
-
-                    svg.append('g')
-                        .attr("class", "y axis")
-                        .call(d3.axisLeft(y));
-
-                    svg.append('g')
-                        .attr('class', 'x axis')
-                        .attr('transform', 'translate(0,' + height + ')')
-                        .call(d3.axisBottom(x))
-
-                    let AppliedLine = d3.line()
-                                .defined(function (d){return d.ProjectedPoints != 'BYE'})
-                                .x(function(d) {return x(d.Week);})
-                                .y(function(d) {return y(d.AppliedPoints);})
-                                .curve(d3.curveMonotoneX)
-
-                    let ProjectedLine = d3.line()
-                                .defined(function (d){return d.ProjectedPoints != 'BYE'})
-                                .x(function(d) {return x(d.Week);})
-                                .y(function(d) {return y(d.ProjectedPoints);})
-                                .curve(d3.curveMonotoneX)
-
-                    svg.append('path')
-                        .datum(playerWeeks)
-                        .attr('class', 'AppliedLine')
-                        .attr('data-legend', 'Applied Points')
-                        .style('stroke', function(){return color(0)})
-                        .on('mousemove', function() {
-                            d3.select(this)
-                                .style('stroke-width', 5);
-                        })
-                        .on('mouseout', function() {
-                            d3.select(this)
-                                .style('stroke-width', 3);
-                        })
-                        .attr('d', AppliedLine)
-
-                    svg.append('path')
-                        .datum(playerWeeks)
-                        .attr('class', 'ProjectedLine')
-                        .attr('data-legend', 'Projected Points')
-                        .style('stroke', function(){return color(1)})
-                        .on('mousemove', function() {
-                            d3.select(this)
-                                .style('stroke-width', 5);
-                        })
-                        .on('mouseout', function() {
-                            d3.select(this)
-                                .style('stroke-width', 3);
-                        })
-                        .attr('d', ProjectedLine)
-
-                    svg.selectAll(".AppliedDot")
-                        .data(playerWeeks)
-                        .enter().append("circle")
-                        .attr('class', 'AppliedDot')
-                        .attr('cx', function(d){return x(d.Week);})
-                        .attr('cy', function(d){return y(d.AppliedPoints);})
-                        .attr('r', '5')
-                        .style('fill', function(){return color(0)})
-                        .on('mousemove', function() {
-                            d3.select(this)
-                                .attr('r', 7);
-                        })
-                        .on('mouseout', function() {
-                            d3.select(this)
-                                .attr('r', '5');
-                        })
-
-                    svg.selectAll(".ProjectedDot")
-                        .data(playerWeeks)
-                        .enter().append("circle")
-                        .attr('class', 'ProjectedDot')
-                        .attr('cx', function(d){return x(d.Week);})
-                        .attr('cy', function(d){return d.ProjectedPoints == 'BYE' ? y(0) :  y(d.ProjectedPoints);})
-                        .attr('r', '5')
-                        .style('fill', function(){return color(1)})
-                        .on('mousemove', function() {
-                            d3.select(this)
-                                .attr('r', 7);
-                        })
-                        .on('mouseout', function() {
-                            d3.select(this)
-                                .attr('r', '5');
-                            
-                        })
-                    
-                    let legend = svg => {
+            // Establish svg legend
+            let legend = svg => {
                         const g = svg
-                            .attr("font-family", "sans-serif")
+                        .attr("font-family", "sans-serif")
                             .attr("font-size", 10)
                             .selectAll("g")
                             .data(color.domain().slice().reverse())
                             .enter().append("g")
-                            .attr("transform", (d, i) => `translate(0,${i * 20})`);
-
+                            .attr("transform", (i) => `translate(0,${i * 20})`);
+                        
                         g.append("circle")
                             .attr("r", 7)
                             .style("fill", color);
@@ -191,12 +74,161 @@
                                 var text = ['Applied Points', 'Projected Points'];
                                 return text[i]
                             });
-                    }
+            }
+            let tooltip = d3.select("body")
+                .append("div")
+                .style("position", "absolute")
+                .style("z-index", "10")
+                .style("visibility", "hidden")
 
-                    svg.append("g")
-                        .attr("transform", `translate(${margin.left + 1},${margin.top})`)
-                        .call(legend);
-            });
+            // Load in the data and format it
+            d3.json('data/PlayerData.json')
+                    .then(function (data) {
+                    
+                        let currPlayer = data[player];
+                        let playerWeeks = currPlayer.Weeks;
+                        let dataPoints = playerWeeks.length
+
+                        // Scrub the data
+                        playerWeeks.forEach(function(d) {
+                            d.Week              = +d.Week;
+                            d.ProjectedPoints   = d.ProjectedPoints == '--' ? 'BYE' : +d.ProjectedPoints;
+                            d.AppliedPoints     = d.AppliedPoints == "--" ? 0 : +d.AppliedPoints;
+                            d.PercentError      = d.PercentError =='--' ? 0 : +d.PercentError;
+                            d.RunningError      = +d.RunningError;
+                        }); 
+
+                        // Establish range of graph
+                        let maxPoints = d3.max(currPlayer.Weeks, function(d){
+                            return d.ProjectedPoints > d.AppliedPoints ? d.ProjectedPoints : d.AppliedPoints;
+                        }) + 1.10;
+
+                        // Establish scales for axes
+                        let x = d3.scaleLinear()
+                                        .domain([0, dataPoints])
+                                        .range([0,width]);
+                        let y = d3.scaleLinear()
+                                        .domain([0, maxPoints])
+                                        .range([height, 0]);
+
+                        // Add a title
+                        svg.append('text')
+                            .attr('x', (width / 2))
+                            .attr('y', 0 - (margin.top / 2))
+                            .attr('text-anchor', 'middle')
+                            .text(player + ', ' + currPlayer.Posistion + " - " + currPlayer.Team)
+                            .attr('class', 'chart-title');
+
+                        // Add the axes
+                        svg.append('g')
+                            .attr('class', 'y axis')
+                            .call(d3.axisLeft(y));
+                        svg.append('g')
+                            .attr('class', 'x axis')
+                            .attr('transform', 'translate(0,' + height + ')')
+                            .call(d3.axisBottom(x));
+
+                        // Define the line functions
+                        let AppliedLine = d3.line()
+                                    .defined(function (d){return d.ProjectedPoints != 'BYE'})
+                                    .x(function(d) {return x(d.Week);})
+                                    .y(function(d) {return y(d.AppliedPoints);})
+                                    .curve(d3.curveMonotoneX)
+                        let ProjectedLine = d3.line()
+                                    .defined(function (d){return d.ProjectedPoints != 'BYE'})
+                                    .x(function(d) {return x(d.Week);})
+                                    .y(function(d) {return y(d.ProjectedPoints);})
+                                    .curve(d3.curveMonotoneX)
+
+                        // Append the lines to the graph
+                        svg.append('path')
+                            .datum(playerWeeks)
+                            .attr('class', 'AppliedLine')
+                            .style('stroke', function(){return color(0)})
+                            .on('mousemove', function(){
+                                d3.select(this)
+                                    .style('stroke-width', 5);
+                            })
+                            .on('mouseout', function(){
+                                d3.select(this)
+                                    .style('stroke-width', 3);
+                            })
+                            .attr('d', AppliedLine)
+                        svg.append('path')
+                            .datum(playerWeeks)
+                            .attr('class', 'ProjectedLine')
+                            .style('stroke', function(){return color(1)})
+                            .on('mousemove', function(){
+                                d3.select(this)
+                                    .style('stroke-width', 5);
+                            })
+                            .on('mouseout', function(){
+                                d3.select(this)
+                                    .style('stroke-width', 3);
+                            })
+                            .attr('d', ProjectedLine)
+
+
+                        // Apply the dots
+                        svg.selectAll('.AppliedDot')
+                            .data(playerWeeks)
+                            .enter().append('circle')
+                            .attr('class', 'AppliedDot')
+                            .attr('cx', function(d){return x(d.Week);})
+                            .attr('cy', function(d){return y(d.AppliedPoints);})
+                            .attr('r', '5')
+                            .style('fill', function(){return color(0);})
+                            .style('display', function(d){
+                                if (d.ProjectedPoints == 'BYE'){
+                                    return 'none';
+                                }
+                                return 'block';
+                            })
+                            .on('mouseover', function(d) {
+                                d3.select(this)
+                                    .attr('r', '7');
+                                return tooltip.style('visibility', 'visible').text(d.AppliedPoints);
+                            })
+                            .on("mousemove", function(){
+                                return tooltip.style("top", (event.pageY-30)+"px").style("left",(event.pageX)+"px");
+                            })
+                            .on('mouseout', function() {
+                                d3.select(this)
+                                    .attr('r', '5');
+                                return tooltip.style("visibility", "hidden");
+                            })
+                        svg.selectAll('.ProjectedDot')
+                            .data(playerWeeks)
+                            .enter().append('circle')
+                            .attr('class', 'AppliedDot')
+                            .attr('cx', function(d){return x(d.Week);})
+                            .attr('cy', function(d){return d.ProjectedPoints == 'BYE' ? y(0) :  y(d.ProjectedPoints);})
+                            .attr('r', '5')
+                            .style('fill', function(){return color(1);})
+                            .style('display', function(d){
+                                if (d.ProjectedPoints == 'BYE'){
+                                    return 'none';
+                                }
+                                return 'block';
+                            })
+                            .on('mouseover', function(d) {
+                                d3.select(this)
+                                    .attr('r', '7');
+                                return tooltip.style('visibility', 'visible').text(d.ProjectedPoints);
+                            })
+                            .on('mousemove', function() {
+                                d3.select(this)
+                                    return tooltip.style("top", (event.pageY-30)+"px").style("left",(event.pageX)+"px");
+                            })
+                            .on('mouseout', function() {
+                                d3.select(this)
+                                    .attr('r', '5');
+                                return tooltip.style("visibility", "hidden");
+                            })
+                        svg.append("g")
+                            .attr("transform", `translate(${margin.left - 20},${margin.top - 20})`)
+                            .call(legend);                    
+                    });
         }
     }
 

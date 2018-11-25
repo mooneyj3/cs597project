f89ba342cbe667dca5c70d55dd9ec276cfd36c3c
Jonny Mooneyham <jonathan.mooneyham@gmail.com>
2018-11-24 19:04:57 -0800

    View updates Tableau embed working now

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
 <template>
     <AppLayout></AppLayout>
-    <!--<div id="app">-->
-        <!--<div id="top-bar">-->
-            <!--<h1>Fluent Fantasy Football</h1>-->
-        <!--</div>-->
-        <!--<nav class="navbar navbar-expand-lg navbar-light bg-light">-->
-            <!--<a class="navbar-brand" href="#">Navbar</a>-->
-            <!--<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">-->
-                <!--<span class="navbar-toggler-icon"></span>-->
-            <!--</button>-->
-            <!--<div class="collapse navbar-collapse" id="navbarNavAltMarkup">-->
-                <!--<div class="navbar-nav">-->
-                    <!--<a class="nav-item nav-link active"><router-link to="/">Home</router-link></a>-->
-                    <!--<router-link to="/about">About</router-link>-->
-                    <!--<router-link to="/jonnysandbox">Jonny's Dev Area</router-link>-->
-                    <!--<router-link to="/austinsandbox">Austin's Dev Area</router-link>-->
-                    <!--<router-link to="/scottsandbox">Scott's Dev Area</router-link>-->
-                    <!--<router-link to="/players">Players</router-link>-->
-                <!--</div>-->
-            <!--</div>-->
-        <!--</nav>-->
-        <!--<router-view/>-->
-    <!--</div>-->
 </template>
 
 <script>
--- src/components/main/Player.vue
+++ src/components/main/Player.vue
@@ -1,45 +1,63 @@
 <template>
-    <v-card>
-        <v-card-title primary-title>
-            <div>
-                <h3 class="headline mb-0">Kerryon Johnson</h3>
-                <div>RB - DET</div>
-            </div>
-        </v-card-title>
-        <v-card-actions>
-            <div id="player-projections">
-                <h2>Projected vs. Actual Points</h2>
-                <PlayerProjectedPoints></PlayerProjectedPoints>
-            </div>
-        </v-card-actions>
-        <v-card-actions>
-            <div id="past-performance">
-                <h2>Past Performance</h2>
-                <PlayerPastPerformanceStackedBar></PlayerPastPerformanceStackedBar>
-            </div>
-        </v-card-actions>
-        <v-card-actions>
-            <div id="teamtreemap">
-                <h2>Player Team Treemap</h2>
-                <p>The following link goes to our treemap prototype on Tableau Public. While we would like to embed this, we are still working through logistics of building in D3, or maintaining an external reference.</p>
-                <a href="https://public.tableau.com/profile/jonathan.mooneyham#!/vizhome/PrototypingNFLData/Sheet1"
-                    target="_blank">
-                    Link to TreeMap in Tableau
-                </a>
-            </div>
-        </v-card-actions>
-        <v-card-actions>
-            <div id="teamdepth">
-                <h2>Team Depth</h2>
-                <!-- <p>The following link takes you to the field view. There were some D3 conflicts integrating this. On the main player page.  The prototype is accessible at teh link below</p>
-                <a href="/austinsandbox" target="_blank">
-                    Link to Football field view with players
-                </a> -->
-                <Depth></Depth>
+    <div id="player">
+        <v-toolbar color="navy blue" dark>
+            <v-toolbar-title>Kerryon Johnson</v-toolbar-title>
+            <v-toolbar-title>RB - DET</v-toolbar-title>
+        </v-toolbar>
+        <v-card>
+            <v-container flued grid-list-lg>
+                <v-layout row wrap>
+                    <!-- Projected vs. Actual -->
+                    <v-flex xs12>
+                        <v-card color="blue-grey darken-2" class="white--text">
+                            <v-card-title primary-title>
+                                <div class="headline">Projected vs. Actual Points</div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <PlayerProjectedPoints></PlayerProjectedPoints>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
 
-            </div>
-        </v-card-actions>
-    </v-card>
+                    <!-- Past Performance -->
+                    <v-flex xs12>
+                        <v-card color="blue-grey darken-2" class="white--text">
+                            <v-card-title primary-title>
+                                <div class="headline">Performance</div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <PlayerPastPerformanceStackedBar></PlayerPastPerformanceStackedBar>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+
+                    <!-- Team Treemap -->
+                    <v-flex xs12>
+                        <v-card color="blue-grey darken-2" class="white--text">
+                            <v-card-title primary-title>
+                                <div class="headline">Team Treemap</div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <TableauTreeMap></TableauTreeMap>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+
+                    <!-- Team Treemap -->
+                    <v-flex xs12>
+                        <v-card color="blue-grey darken-2" class="white--text">
+                            <v-card-title primary-title>
+                                <div class="headline">Team Depth</div>
+                            </v-card-title>
+                            <v-card-actions>
+                                <Depth></Depth>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
+    </div>
 </template>
 
 <script>
@@ -51,8 +69,7 @@
     export default {
         name: "Player",
         components: {TableauTreeMap, PlayerProjectedPoints, PlayerPastPerformanceStackedBar, Depth},
-        methods: {
-        },
+        methods: {},
         mounted() {
         }
     }
